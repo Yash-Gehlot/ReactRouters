296c85f389c8770f37ba580105e4bbd1ac61bce0
Yash Gehlot <yashgehlot1233@gmail.com>
2024-04-23 19:37:48 +0100

    React Routers

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='p-4 m-4 text-3xl text-center bg-slate-500'>Github Followers
+    </div>
+  )
+}
+
+export default Github
